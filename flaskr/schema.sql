c86e256f0d728a20a4dc90b2f40923b059603d21
yamato-1205 <ysakata1205@gmail.com>
2022-05-21 10:17:22 +0900

    make sql

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,17 +1,38 @@
 DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS groupe;
+DROP TABLE IF EXISTS task;
+DROP TABLE IF EXISTS conection;
 
 CREATE TABLE user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT UNIQUE NOT NULL,
+    username TEXT NOT NULL,
     password TEXT NOT NULL
 );
 
-CREATE TABLE post (
+CREATE TABLE groupe (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    author_id INTEGER NOT NULL,
+    groupename TEXT NOT NULL,
+    groupecode CHAR[(8)] NOT NULL
+);
+
+CREATE TABLE task (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    groupe_id INTEGER NOT NULL,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
-    FOREIGN KEY (author_id) REFERENCES user (id)
+    FOREIGN KEY (groupe_id) REFERENCES groupe (id)
 );
+
+CREATE TABLE conection (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    groupe_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user (id),
+    FOREIGN KEY (groupe_id) REFERENCES groupe (id)
+);
+
+INSERT INTO user (username, password) VALUES ('yamato', '1015');
+INSERT INTO groupe (groupename, groupecode) VALUES ('NMB', '14932534');
+INSERT INTO task (groupe_id, title, body) VALUES (1, '柵運ぶ', '100本');
+INSERT INTO conection (user_id, groupe_id) VALUES (1, 1);
