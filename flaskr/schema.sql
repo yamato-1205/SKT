486a267ac2317ea85fae513e64c98941041fe05a
yamato-1205 <ysakata1205@gmail.com>
2022-05-22 04:41:46 +0900

    データベースの初期値を設定

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -35,8 +35,7 @@ CREATE TABLE conection (
     FOREIGN KEY (groupe_id) REFERENCES groupe (id)
 );
 
-INSERT INTO user (username, password) VALUES ('yamato', '1015');
-INSERT INTO groupe (groupe_name, groupe_explain, groupe_code) VALUES ('NMB', '4月3日〜4月5日', '1');
-INSERT INTO groupe (groupe_name, groupe_explain, groupe_code) VALUES ('ベリーグッドマン', '私とあいつの最強計画', '3');
-INSERT INTO task (groupe_id, title, body) VALUES (1, '柵運ぶ', '100本');
-INSERT INTO conection (user_id, groupe_id) VALUES (1, 1);
+INSERT INTO user (username, password) VALUES ('yamato', '1111'), ('sakata', '1111'), ('山田', '1111'), ('田中', '1111');
+INSERT INTO groupe (groupe_name, groupe_explain, groupe_code) VALUES ('初めてのハッカソン', '5月21日〜5月22日 @オンライン', '1'), ('プロジェクトA', '6月までに完成目標', '2'), ('プロジェクトB', '8月までに完成目標', '3'), ('送別会', 'サークルの先輩たちへの色紙づくり', '4');
+INSERT INTO task (groupe_id, title, body) VALUES (1, 'ログイン機能', 'メールアドレスは不要'), (1, 'ログアウト機能', 'ポップアップによる確認必須'), (1, 'スライド作成', '最終日の16時までに完成'), (4, '佐藤先輩の写真集め', 'これだけなる早でよろしく');
+INSERT INTO conection (user_id, groupe_id) VALUES (1, 1), (1, 2), (1, 3), (2, 1), (2, 3), (3, 1), (3, 2), (3, 4), (4, 1), (4, 2);
